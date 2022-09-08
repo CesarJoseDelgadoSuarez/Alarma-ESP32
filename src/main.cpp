a8e96c548d7e1623bf4d2b3844b741874457e400
César José Delgado Suárez <cjdsgithub@gmail.com>
2022-09-08 13:36:32 +0100

    añadido sensor24h y arreglado bug contraseña

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,11 @@ int passIntroducida[]={0,0,0,0};
 int indicePassIntroducida = 0;
 int columnaAsteriscoPass = 8;
 
+//Pins led 
+#define ledAlarmaOn 27
+#define ledAlarmaOff 33
+#define ledSirena 26
+
 // Pins a los que los sensores estan conectados.
 #define pinSensor24h 14
 #define pinSensor2 16
@@ -42,118 +47,166 @@ EasyButton sensor8(pinSensor8);
 bool alarmaOn = false;  // Variable que indica si la alarma esta conectada o no
 bool awayOn = false;    // Variable que indica si la alarma esta conectada en AWAY
 bool stayOn = false;    // Variable que indica si la alarma esta conectada en STAY
+bool passCorrecta=false;
 
-
-
+//bool sensores
+bool sensor24hDisparado = false;
 // Variables de tiempo
 unsigned long tiempoCuentaAtrasSalidaSegundos = 5;  // Variable que indica el tiempo para salir del domicilio tras conectar la alarma
 
-// Metodo-> acciones que realiza ESP32 al activarse el sensor 24h
-void sensor24hPresionado()
-{
+// Metodo -> Imprimir mensaje de alarma conectada
+void imprimirMensajeAlarmaconectada(String m,int fila, int columna){
+  lcd.setCursor(columna,fila);
+  lcd.print("                    ");
+  lcd.setCursor(columna,fila);
+  lcd.print(m);
+} 
+
+//Metodo -> Enciende los leds de la alarma
+void ledsOn(){
+  Serial.println("ledsOn()");
+  digitalWrite(ledAlarmaOff,LOW);
+  digitalWrite(ledAlarmaOn,HIGH);
+}
+
+//Metodo -> Apaga los leds de la alarma
+void ledsOff(){
+  Serial.println("ledsOff()");
+  digitalWrite(ledAlarmaOff,HIGH);
+  digitalWrite(ledAlarmaOn,LOW);
+}
+
+//Metodo -> Enciende la alarma
+void encenderAlarma(){
+  alarmaOn=true;
+  //LEDS ON
+  ledsOn();
+  Serial.println("Encender Alarma()");
+  String m;
+  if (awayOn)
+  {
+    m = "     Alarma AWAY     ";
+  }else{
+    m = "     Alarma STAY     ";
+  }
+  
+  imprimirMensajeAlarmaconectada(m,0,0);
+}
+
+// Metodo -> Enciende la alarma tras ser activado el sensor 24h
+void encenderAlarma24HPresionado(){
 
 }
-// Metodo-> acciones que realiza ESP32 al activarse el sensor 2
+// Metodo -> acciones que realiza ESP32 al activarse el sensor 24h
+void sensor24hPresionado() // FUNCION PARA H24
+{
+  sensor24hDisparado = true;
+  alarmaOn = true;
+  awayOn = true;
+  stayOn = true;
+  encenderAlarma24HPresionado();
+  ledsOn();
+
+}
+
+// Metodo -> acciones que realiza ESP32 al activarse el sensor 2
 void sensor2Presionado()
 {
 
 }
-// Metodo-> acciones que realiza ESP32 al activarse el sensor 3
+// Metodo -> acciones que realiza ESP32 al activarse el sensor 3
 void sensor3Presionado()
 {
 
 }
-// Metodo-> acciones que realiza ESP32 al activarse el sensor 4
+// Metodo -> acciones que realiza ESP32 al activarse el sensor 4
 void sensor4Presionado()
 {
 
 }
-// Metodo-> acciones que realiza ESP32 al activarse el sensor 5
+// Metodo -> acciones que realiza ESP32 al activarse el sensor 5
 void sensor5Presionado()
 {
 
 }
-// Metodo-> acciones que realiza ESP32 al activarse el sensor 6
+// Metodo -> acciones que realiza ESP32 al activarse el sensor 6
 void sensor6Presionado()
 {
 
 }
-// Metodo-> acciones que realiza ESP32 al activarse el sensor 7
+// Metodo -> acciones que realiza ESP32 al activarse el sensor 7
 void sensor7Presionado()
 {
 
 }
-// Metodo-> acciones que realiza ESP32 al activarse el sensor 8
+// Metodo -> acciones que realiza ESP32 al activarse el sensor 7
+void sensor8Presionado(){
+
+}
+// Metodo -> acciones que realiza ESP32 al activarse el sensor 8
 void sensor8Secuencia()
 {
 
 }
 
-// Metodo-> interrupcion del sensor de 24h
+// Metodo -> interrupcion del sensor de 24h
 void interrupcionSensor24h()
 {
   sensor24h.read();
 }
 
-// Metodo-> interrupcion del sensor de 2
+// Metodo -> interrupcion del sensor de 2
 void interrupcionSensor2()
 {
   sensor2.read();
 }
 
-// Metodo-> inicializa todas las varibles de EasyButton referidas a los sensores de la alarma
+// Metodo -> inicializa todas las varibles de EasyButton referidas a los sensores de la alarma
 void setupSensores(){
-  //SENSOR 24 HORAS
-  sensor24h.begin();
+  sensor24h.begin(); 
   sensor24h.onPressed(sensor24hPresionado);
   if (sensor24h.supportsInterrupt())
   {
     sensor24h.enableInterrupt(interrupcionSensor24h);
-    Serial.println("Sensor24h habilitado para interrupciones");
   }
 
-  //SENSOR 2
   sensor2.begin();
   sensor2.onPressed(sensor2Presionado);
   if (sensor2.supportsInterrupt())
   {
     sensor2.enableInterrupt(interrupcionSensor2);
-    Serial.println("sensor2 habilitado para interrupciones");
   }
-
-  //SENSOR 3
-  sensor3.begin();
-  sensor3.onPressed(sensor3Presionado);
-
-  //SENSOR 4
-  sensor4.begin();
+  
+  sensor3.begin(); 
+  sensor3.onPressed(sensor3Presionado); 
+  
+  sensor4.begin(); 
   sensor4.onPressed(sensor4Presionado);
-
-  //SENSOR 5
-  sensor5.begin();
+  
+  
+  sensor5.begin(); 
   sensor5.onPressed(sensor5Presionado);
-
-  //SENSOR 6
+  
+  
   sensor6.begin();
   sensor6.onPressed(sensor6Presionado);
-
-  //SENSOR 7
+  
+  
   sensor7.begin();
   sensor7.onPressed(sensor7Presionado);
+  
+  sensor8.begin(); 
+  sensor8.onPressed(sensor8Presionado);
+  sensor8.onSequence(2, 3000, sensor8Secuencia);
 
-  //SENSOR 8
-  sensor8.begin();
-  sensor8.onSequence(2,3000,sensor8Secuencia);
-  //  sensor24h.onSequence(2, 1500, sequenceEllapsed);
 }
 
-// Metodo-> inicializa la variable lcd
+// Metodo -> inicializa la variable lcd
 void setupLCD(){
   lcd.init();
-  lcd.backlight();
 }
 
-// Meotodo-> inicializa las variables del teclado
+// Meotodo -> inicializa las variables del teclado
 void setupTeclado(){
   Wire.begin();
   Wire.setClock(400000);
@@ -166,11 +219,19 @@ void setupTeclado(){
   teclado.loadKeyMap(mapaTeclado);
 }
 
-// Metodo-> imprime la pantalla de inicio de la alarma
+// Metodo -> imprime la pantalla de inicio de la alarma
 void pantallaInicio(){
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("* o # para alarma on");
+}
+
+// Metodo -> inicializa 
+void setupLedsAlarma(){
+  pinMode(ledAlarmaOff,OUTPUT);
+  pinMode(ledAlarmaOn,OUTPUT);
+  digitalWrite(ledAlarmaOff,HIGH);
+  digitalWrite(ledAlarmaOn,LOW);
 }
 
 void setup()
@@ -189,6 +250,9 @@ void setup()
 
   //pantalla inicio lcd
   pantallaInicio();
+
+  //leds Alarma
+  setupLedsAlarma();
 }
 
 // Metodo-> realiza una espera activa de un tiempo t para seguir escuchando interrupciones
@@ -196,6 +260,7 @@ void esperaActiva(unsigned long tiempoEspera){
   unsigned long t = millis();
   while (millis() - t <= tiempoEspera);
 }
+
 // Metodo-> devuelve el numero pulsado, en caso contrario devuelve E
 char leerSoloNumeros(){
   char c = teclado.getChar();
@@ -214,6 +279,25 @@ bool compararPass(){
     }
   }
   return true;
+}
+
+// // Metodo-> resetea la contraseña introducida 
+// bool resetearPass(){
+//   for(int i = 0; i < sizeof(pass)/sizeof(pass[0]);i++){//sizeof() devuelve el numero de bytes total del array. para obtener cuantos elementos hay que dividir los bytes totales / byte de cada elemento
+//     passIntroducida[i] = 0;
+//   }
+//   return true;
+// }
+
+// Metodo -> Apaga la alarma solo si la contraseña se ha introducido correctamente
+void apagarAlarma(){
+  alarmaOn=false;
+  awayOn=false;
+  stayOn=false;
+  passCorrecta=false;
+  ledsOff();
+  pantallaInicio();
+  //resetearPass();
 }
 
 // Metodo-> comprueba si se ha introducido la contraseña
@@ -237,15 +321,13 @@ void comprobarPass(){
         lcd.print("    ");
         columnaAsteriscoPass=8;
         indicePassIntroducida=0;
-
+        passCorrecta=false;
       }else{
         Serial.println("Contraseña Correcta!!");
         columnaAsteriscoPass=8;
         indicePassIntroducida=0;
-        alarmaOn=false;
-        awayOn=false;
-        stayOn=false;
-        pantallaInicio();
+        passCorrecta=true;
+        apagarAlarma();
       }
     }
     
@@ -256,16 +338,14 @@ void comprobarPass(){
 
 // Metodo-> realiza la cuenta atras dado un tiempo
 void cuentaAtras(String m, unsigned long tiempoReferencia, int segundosRestantes){
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(m);
+  imprimirMensajeAlarmaconectada(m,0,0);
   Serial.println(m);
   for (int i = segundosRestantes; i >= 0; i--)
   {
     while (millis()-tiempoReferencia <= 1000){
       comprobarPass();
-    };//dentro debe mirar si se introduce contraseña para cancelar encendido de la alarma
-    if (!alarmaOn)break;
+    };
+    if (passCorrecta)break;
     
     Serial.println(i);
     lcd.setCursor(17,0);
@@ -275,33 +355,28 @@ void cuentaAtras(String m, unsigned long tiempoReferencia, int segundosRestantes
   tiempoReferencia = millis();
   while (millis() - tiempoReferencia <= 1000){
     comprobarPass();
-    if (!alarmaOn)break;
+    if (!passCorrecta)break;
     
-  }
-  if(alarmaOn){
-    lcd.setCursor(0,0);
-    lcd.print("  Alarma Conectada  ");
-  }
-  
+  }  
 }
 
 void revisarBotonesEncendido(){
   char c = teclado.getChar();
   if (c=='*')//Alarma activada en AWAY
   {
-    alarmaOn = true;
+    cuentaAtras("Alarma en AWAY:",millis(),tiempoCuentaAtrasSalidaSegundos);
     awayOn = true;
     stayOn = false;
-    cuentaAtras("Alarma en AWAY:",millis(),tiempoCuentaAtrasSalidaSegundos);
-    
+    encenderAlarma();
   }
 
   if (c=='#')//Alarma activada en STAY
   {
-    alarmaOn = true;
+    cuentaAtras("Alarma en STAY:",millis(),tiempoCuentaAtrasSalidaSegundos);
     awayOn = false;
     stayOn = true;
-    cuentaAtras("Alarma en STAY:",millis(),tiempoCuentaAtrasSalidaSegundos);
+    encenderAlarma();
+    
   }
   
 }
@@ -312,6 +387,11 @@ void loop()
   {
     revisarBotonesEncendido();
   }else{
+    if (sensor24hDisparado)
+    {
+      imprimirMensajeAlarmaconectada("   Alarma On Away   ",0,0);
+    }
+    
     comprobarPass();
   }
 }
